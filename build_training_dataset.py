b0dbf0a4386bfb8f75073bc284eb76886d8c8dd4
Henry Rossiter <henry.rossiter@utexas.edu>
2018-03-06 14:24:51 -0600

    quick fix

--- build_training_dataset.py
+++ build_training_dataset.py
@@ -1,4 +1,3 @@
-import numpy as np
 import pandas as pd
 
 
@@ -6,7 +5,22 @@ training_df = pd.DataFrame(columns=['year','winning_team_id','losing_team_id','s
 tournament_game_results_df = pd.read_csv('input/NCAATourneyCompactResults.csv')
 seeds_df = pd.read_csv('input/NCAATourneySeeds.csv')
 
+def matchup_to_string(team1_num, team2_num, team1_seed, team2_seed, team1_presence_last_year, team2_presence_last_year, exp_diff):
+	print("Team 1: "+str(team1_num)+"	seed: "+str(team1_seed)+"	in tourney last year: "+str(team1_presence_last_year))
+	print("Team 2: "+str(team2_num)+"	seed: "+str(team2_seed)+"	in tourney last year: "+str(team2_presence_last_year))
+	print("Seed differential: "+str(team2_seed - team1_seed)+" -- presence differential: "+str(exp_diff)+'\n')
+	
+#def prior_avg_ppg(team_id, up_to_year):
+	
 for game in range(len(tournament_game_results_df.index)):
-    curr_row = tournament_game_results_df.iloc[game]
-    year = curr_row['Season']
-    print(year)
+	curr_row = tournament_game_results_df.iloc[game]
+	year = curr_row['Season']
+	w_team_id = curr_row['WTeamID']
+	l_team_id = curr_row['LTeamID']
+	w_team_seed = seeds_df.loc[seeds_df['Season'] == year and seeds_df['TeamID'] == w_team_id].Seed[1:] #string
+	l_team_seed = seeds_df.loc[seeds_df['Season'] == year and seeds_df['TeamID'] == l_team_id].Seed[1:] #string
+	seed_diff = int(l_team_seed) - int(w_team_seed)
+	w_team_in_tourney_last_year = (seeds['Season'] == year and seeds['TeamID'] == w_team_id)
+	l_team_in_tourney_last_year = (seeds['Season'] == year and seeds['TeamID'] == l_team_id)
+	exp_diff = int(w_team_in_tourney_last_year) - int(l_team_in_tourney_last_year)
+	matchup_to_string(w_team_id, l_team_id, w_team_seed, l_team_seed, w_team_in_tourney_last_year, l_team_in_tourney_last_year, exp_diff)	
